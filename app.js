b106158050e648648448148dea67aa955b39171d
Chris Gagnon <foto2mn@comcast.net>
2026-07-28 13:40:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -17,14 +17,6 @@ const TOTE_ITEMS = [
 },
 
 {
-    partNumber: "214181",
-    category: "ONT",
-    description: "ADTRAN 411 MICRO ONT",
-    shorthand: "411",
-    maxQty: 0
-},
-    
-{
     partNumber: "213567",
     category: "ONT",
     description: "ADTRAN SDX611 ONT 1GE C-Chip",
